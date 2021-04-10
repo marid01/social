5969e88af38cc692b18e40be44303ae034de495d
Marid <maridshikhkerimov@gmail.com>
2021-04-10 21:07:08 +0300

    Social
    Social

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Header from "./ components/Header";
+import Nav from "./ components/Navbar";
+import Profile from "./ components/Profile";
+
+const App = () => {
+    return (
+        <div className="app-wrapper">
+            <Header/>
+            <div className='con'>
+                <Nav/>
+                <Profile />
+
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+type HeaderType = {
+}
+
+const Header: React.FC<HeaderType> = (props) => {
+    return (
+        <header className='header'>
+            <h1>sdsdsdsasd</h1>
+        </header>
+    )
+}
+
+export default Header
